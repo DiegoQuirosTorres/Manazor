35848742be817c15bba2401cba48b8d54c1c44ff
dquitor630 <diegoquirostorres26@gmail.com>
2023-11-27 20:47:22 +0100

    Query de products

---
+++ Manazor.Application/Features/Products/Queries/GetAllProductsDto.cs
@@ -0,0 +1,28 @@
+﻿using Manazor.Application.Common.Mappings;
+using Manazor.Domain.Entities.ProductModule;
+using Manazor.Domain.Entities.WarehouseModule;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manazor.Application.Features.Products.Queries
+{
+    public class GetAllProductsDto : IMapFrom<Product>
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public byte[]? Photo { get; set; }
+
+        public int LowQuantity { get; set; }
+
+        public int Category { get; set; } = 1;
+
+        public decimal Price { get; set; }
+    }
+}
